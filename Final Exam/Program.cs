5824864e6be3aa08678683a0f053caffe3a879c5
RiK <rik_for_all@mail.ru>
2023-05-20 15:30:33 +0600

    Added Final Exam folder with C# code

---
+++ Final Exam/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] originalArray = { "hello", "world", "this", "is", "a", "test", "for", "exam" };
+
+string[] newArray = new string[originalArray.Length];
+int index = 0;
+
+for (int i = 0; i < originalArray.Length; i++)
+{
+    if (originalArray[i].Length <= 3)
+    {
+        newArray[index] = originalArray[i];
+        index++;
+    }
+}
+
+Console.WriteLine("New array:");
+for (int i = 0; i < index; i++)
+{
+    Console.WriteLine(newArray[i]);
+}
+
+Console.ReadLine();
